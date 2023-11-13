6216381986eaa27faa7b8b63487fefa35ee1897f
Georgy <g.batanov@makves.ru>
2023-11-13 16:15:08 +0300

    v0.0.8 use Goview

--- demogin/main.go
+++ demogin/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -11,10 +12,12 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/foolin/goview"
+	"github.com/foolin/goview/supports/ginview"
 	"github.com/gin-gonic/gin"
 )
 
-const VERSION = "0.0.7"
+const VERSION = "0.0.8"
 const SERVER = "192.168.76.95:8089"
 
 // Пример встраивания целой папки в переменную
@@ -46,18 +49,37 @@ func main() {
 	//	router := gin.Default()
 	router := gin.New()
 
-	// Используем встроенные шаблоны (включаются в тело программы)
-	templ := template.Must(template.New("").ParseFS(f, "html/tpl/*.tmpl"))
-	router.SetHTMLTemplate(templ)
-	// Использования шаблонов из ФС
-	//	  router.LoadHTMLGlob(".\\html\\tpl\\*")
-	//
+	// Используем Goview (Ginview - вариант для Gin)
+	var vConfig goview.Config = goview.Config{
+		Root:      "html/tpl",       //template root path
+		Extension: ".tmpl",          //file extension
+		Master:    "layouts/master", //master layout file
+		//		Partials:  []string{"partials/head"}, //partial files
+		// Глобальная функция, передаваемая в шаблон
+		Funcs: template.FuncMap{
+			"sub": func(a, b int) int {
+				return a - b
+			},
+			// more funcs
+		},
+		DisableCache: true, //if disable cache, auto reload template file for debug.
+		//		Delims:       Delims{Left: "{{", Right: "}}"},
+	}
+	router.HTMLRender = ginview.New(vConfig)
+	/*
+		// Используем встроенные шаблоны (включаются в тело программы)
+		templ := template.Must(template.New("").ParseFS(f, "html/tpl/*.tmpl"))
+		router.SetHTMLTemplate(templ)
+		// Использования шаблонов из ФС
+		//	  router.LoadHTMLGlob(".\\html\\tpl\\*")
+		//
+	*/
 	//router.StaticFS("/css", http.FS(f)) // Не заработало /html /html/css /css
-	router.StaticFS("/css", http.Dir("html/css")) // так работает, но это не embed
+	router.StaticFS("/css", gin.Dir("html/css", false)) // так работает, но это не embed?
 	//router.Static("/css", ".\\html\\css") // так работает
 
 	// картинка
-	router.StaticFile("/img/fav32.png", ".\\html\\img\\fav32.png")
+	router.StaticFile("/img/fav32.png", "./html/img/fav32.png")
 	// иконка из файла
 	//	router.StaticFile("/favicon.ico", ".\\html\\img\\fav32.png")
 	// иконка из внедренной картинки
@@ -75,17 +97,29 @@ func main() {
 	router.GET("/cmd", actionSrv.cmdHandler)
 	router.GET("/", actionSrv.mainPage)
 
+	// Кастомный логгер
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s -  %s\"\n",
+			param.TimeStamp.Format(time.RFC1123),
+			param.ErrorMessage,
+		)
+	}))
+
+	router.Use(gin.Recovery()) // Восстанавливает сервер после panic error
+
 	//	log.Printf("%v", router.Routes())
 	srv := &http.Server{
 		Addr:    SERVER,
 		Handler: router,
 	}
+
+	// Старт сервера
 	go func() {
-		// service connections
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}() // listen and serve
+
 	<-quit
 	log.Println("Shutdown Server ...")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -118,5 +152,13 @@ func (ts *ActionHandler) cmdHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "command.tmpl", gin.H{"title": "GSB website", "id": id, "cmd": cmd})
 }
 func (ts *ActionHandler) mainPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "GSB website"})
+	ginview.HTML(c, http.StatusOK, "index", gin.H{
+		"title": "GSB website",
+		"add": func(a int, b int) int {
+			return a + b
+		},
+		"sub": func(a, b int) int {
+			return a - b
+		},
+	})
 }
