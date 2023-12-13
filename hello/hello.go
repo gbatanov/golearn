78c3a87b54a76f5c776a261d17cb2dcc8b1d8881
admin <user@AstraOrel>
2023-12-13 12:51:55 +0300

    v0.2.13 hello

--- hello/hello.go
+++ hello/hello.go
@@ -3,20 +3,15 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"crypto/sha256"
 	b64 "encoding/base64"
 	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"gbatanov/hello/nfs"
-	"gbatanov/hello/nfs/rpc"
-	"gbatanov/hello/nfs/util"
 
 	"io"
 	"io/fs"
-	"io/ioutil"
 	"log"
 	"math"
 	"math/rand"
@@ -41,7 +36,7 @@ import (
 	"github.com/matishsiao/goInfo"
 )
 
-const Version string = "v0.2.12"
+const Version string = "v0.2.13"
 
 var Os string = ""
 
@@ -925,12 +920,14 @@ func Generator() chan int {
 			// select блокируется до тех пор, пока один из его блоков case не будет готов к запуску,
 			// а затем выполняет этот блок. Если сразу несколько блоков могут быть запущены,
 			// то выбирается произвольный.
+			// На Windows порядок селектов неважен, на AstraLinux  первым егадо ставить чтение из канала,
+			// иначе возникает panic error
 			select {
-			case ch <- n: // после закрытия канала этот case не будет выполняться,
-				// поэтому паники от записи в закрытй канал не возникнет
-				n++
 			case <-ch:
 				return
+			case ch <- n: // после закрытия канала этот case не будет выполняться,
+				// поэтому паники от записи в закрытый канал не возникнет
+				n++
 			}
 		}
 	}()
@@ -2538,7 +2535,7 @@ func funcRandom() {
 
 // в Windows не работает
 func funcSyslog() {
-	fmt.Println("\nЗапись в syslog")
+	log.Println("\nЗапись в syslog")
 	/*
 		sysLog, err := syslog.New(syslog.LOG_WARNING|syslog.LOG_LOCAL7, "gsb_tag")
 		if err != nil {
@@ -2551,191 +2548,6 @@ func funcSyslog() {
 		// читаем в логе:
 		// cat /var/log/local7.log | grep gsb_tag
 	*/
-}
-func funcConnectToShare() error {
-	/*
-		111 TCP/UDP = RPCBIND/Portmapper ;
-		635 TCP/UDP = mountd ;
-		2049 TCP/UDP = nfs ;
-		4045 TCP/UDP = nlockmgr (только для NFS версии 3);
-		4046 TCP/UDP = status (только для NFS версии 3).
-	*/
-	fmt.Println("\nNFS")
-	var serverIp string = "192.168.8.115"
-	var share string = "/SVM02_NFS"
-	util.DefaultLogger.SetDebug(true)
-	util.Infof("host=%s target=%s\n", serverIp, share)
-
-	mount, err := nfs.DialMount(serverIp)
-	if err != nil {
-		log.Fatalf("unable to dial MOUNT service: %v", err)
-	}
-	//	defer mount.Close()
-
-	auth := rpc.NewAuthUnix("unknown", 0, 3)
-	auth.Gids = 3
-	auth.Stamp = 0xaaaaaaaa
-
-	v, err := mount.Mount(share, auth.Auth()) // ошибка здесь
-	if err != nil {
-		log.Fatalf("unable to mount volume: %v", err)
-	}
-	//defer v.Close()
-
-	// discover any system files such as lost+found or .snapshot
-	dirs, err := ls(v, ".")
-	if err != nil {
-		log.Fatalf("ls: %s", err.Error())
-	}
-	baseDirCount := len(dirs)
-	fmt.Println(baseDirCount)
-
-	/*
-		// check the length.  There should only be 1 entry in the target (aside from . and .., et al)
-		if len(dirs) != 1+baseDirCount {
-			log.Fatalf("expected %d dirs, got %d", 1+baseDirCount, len(dirs))
-		}
-
-		// 10 MB file
-		if err = testFileRW(v, "10mb", 10*1024*1024); err != nil {
-			log.Fatalf("fail")
-		}
-
-		// 7b file
-		if err = testFileRW(v, "7b", 7); err != nil {
-			log.Fatalf("fail")
-		}
-
-		// should return an error
-		if err = v.RemoveAll("7b"); err == nil {
-			log.Fatalf("expected a NOTADIR error")
-		} else {
-			nfserr := err.(*nfs.Error)
-			if nfserr.ErrorNum != nfs.NFS3ErrNotDir {
-				log.Fatalf("Wrong error")
-			}
-		}
-
-		if err = v.Remove("7b"); err != nil {
-			log.Fatalf("rm(7b) err: %s", err.Error())
-		}
-
-		if err = v.Remove("10mb"); err != nil {
-			log.Fatalf("rm(10mb) err: %s", err.Error())
-		}
-
-		_, _, err = v.Lookup(dir)
-		if err != nil {
-			log.Fatalf("lookup error: %s", err.Error())
-		}
-
-		if _, err = ls(v, "."); err != nil {
-			log.Fatalf("ls: %s", err.Error())
-		}
-
-		if err = v.RmDir(dir); err == nil {
-			log.Fatalf("expected not empty error")
-		}
-
-		for _, fname := range []string{"/one", "/two", "/a/one", "/a/two", "/a/b/one", "/a/b/two"} {
-			if err = testFileRW(v, dir+fname, 10); err != nil {
-				log.Fatalf("fail")
-			}
-		}
-
-		if err = v.RemoveAll(dir); err != nil {
-			log.Fatalf("error removing files: %s", err.Error())
-		}
-
-		outDirs, err := ls(v, ".")
-		if err != nil {
-			log.Fatalf("ls: %s", err.Error())
-		}
-
-		if len(outDirs) != baseDirCount {
-			log.Fatalf("directory should be empty of our created files!")
-		}
-
-		if err = mount.Unmount(); err != nil {
-			log.Fatalf("unable to umount target: %v", err)
-		}
-	*/
-	//mount.Close()
-	util.Infof("Completed tests")
-
-	return nil
-}
-
-func testFileRW(v *nfs.Target, name string, filesize uint64) error {
-
-	// create a temp file
-	f, err := os.Open("/dev/urandom")
-	if err != nil {
-		util.Errorf("error openning random: %s", err.Error())
-		return err
-	}
-
-	wr, err := v.OpenFile(name, 0777)
-	if err != nil {
-		util.Errorf("write fail: %s", err.Error())
-		return err
-	}
-
-	// calculate the sha
-	h := sha256.New()
-	t := io.TeeReader(f, h)
-
-	// Copy filesize
-	n, err := io.CopyN(wr, t, int64(filesize))
-	if err != nil {
-		util.Errorf("error copying: n=%d, %s", n, err.Error())
-		return err
-	}
-	expectedSum := h.Sum(nil)
-
-	if err = wr.Close(); err != nil {
-		util.Errorf("error committing: %s", err.Error())
-		return err
-	}
-
-	//
-	// get the file we wrote and calc the sum
-	rdr, err := v.Open(name)
-	if err != nil {
-		util.Errorf("read error: %v", err)
-		return err
-	}
-
-	h = sha256.New()
-	t = io.TeeReader(rdr, h)
-
-	_, err = ioutil.ReadAll(t)
-	if err != nil {
-		util.Errorf("readall error: %v", err)
-		return err
-	}
-	actualSum := h.Sum(nil)
-
-	if bytes.Compare(actualSum, expectedSum) != 0 {
-		log.Fatalf("sums didn't match. actual=%x expected=%s", actualSum, expectedSum) //  Got=0%x expected=0%x", string(buf), testdata)
-	}
-
-	log.Printf("Sums match %x %x", actualSum, expectedSum)
-	return nil
-}
-
-func ls(v *nfs.Target, path string) ([]*nfs.EntryPlus, error) {
-	dirs, err := v.ReadDirPlus(path)
-	if err != nil {
-		return nil, fmt.Errorf("readdir error: %s", err.Error())
-	}
-
-	util.Infof("dirs:")
-	for _, dir := range dirs {
-		util.Infof("\t%s\t%d:%d\t0%o", dir.FileName, dir.Attr.Attr.UID, dir.Attr.Attr.GID, dir.Attr.Attr.Mode)
-	}
-
-	return dirs, nil
 }
 
 func funcDb() {
